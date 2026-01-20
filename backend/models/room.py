d5def91b141f79834e67d3c865baeb0790a5d2d4
nq1s788 <dchsk@inbox.ru>
2026-01-20 16:42:05 +0300

    fix

--- backend/models/room.py
+++ backend/models/room.py
@@ -5,7 +5,7 @@ from .database import Base
 class Room(Base):
     __tablename__ = "Rooms"
 
-    id = Column(Integer, primary_key=True, notnull=True)
+    id = Column(Integer, primary_key=True, nullable=False)
     pan_id = Column(String)
     host_email = Column(String, foreign_key="Users.email")
 
--- backend/models/user.py
+++ backend/models/user.py
@@ -5,8 +5,8 @@ from .database import Base
 class User(Base):
     __tablename__ = "Users"
 
-    id = Column(Integer, primary_key=True, notnull=True)
-    email = Column(String, notnull=True, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False)
+    email = Column(String, nullable=False, unique=True)
     elo = Column(Integer)
     matches = Column(Integer)
     coord = Column(String)
