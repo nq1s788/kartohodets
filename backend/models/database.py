7c8f0371aa3441345a95468f181d81cf16600d04
nq1s788 <58694878+nq1s788@users.noreply.github.com>
2026-09-04 22:55:38 +0500

    Update database.py

--- backend/models/database.py
+++ backend/models/database.py
@@ -2,16 +2,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from backend.config import config
 
-# Create engine
 engine = create_engine(config.DATABASE_URL)
 
-# Create session
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-# Base class for models
 Base = declarative_base()
 
-# Dependency for DB sessions
 def get_db():
     db = SessionLocal()
     try:
